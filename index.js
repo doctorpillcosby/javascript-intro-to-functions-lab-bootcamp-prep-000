ac22bb19f03c9103c607a02b00792a52bb5d7c6e
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-24 16:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logShout(string){
   }
   
 function sayHitoGrandma(sring){
-  if (string.toLowerCase === string) {return}
+  if (string.toLowerCase === string) {return "I can't hear you!"}
   
 }
